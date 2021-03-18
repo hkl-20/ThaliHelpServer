6aa577ccf78ad8aac4554995dfca755042cd7914
hkl-20 <hl04349@st.habib.edu.pk>
2021-03-18 10:08:10 +0500

    some changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const actions = require('../methods/actions')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('ThaliHelp Server')
 })
 
 router.get('/dashboard', (req, res) => {
